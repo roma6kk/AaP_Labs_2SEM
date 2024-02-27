f397a8223c2e178248207e75e6298f27b4f899ff
Roman <123767751+roma6kk@users.noreply.github.com>
2024-02-27 20:03:41 +0300

    Add files via upload

---
+++ Lab3/Lab3E7.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<Windows.h>
+#include<string>
+#include<fstream>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	string line;
+	fstream FILE1("FILE1.txt");
+	if (!FILE1.is_open())
+	{
+		cerr << "Ошибка открытия файла FILE1.txt! " << endl;
+		return 1;
+	}
+	fstream FILE2("FILE2.txt");
+	if (!FILE2.is_open())
+	{
+		cerr << "Ошибка открытия файла FILE1.txt! " << endl;
+		return 1;
+	}
+	while (getline(FILE1, line))
+	{
+		bool is_num = false;
+		for (int i = 0; i < line.length(); i++)
+		{
+			if (line[i] >= '0' && line[i] <= '9')
+			{
+				is_num = true;
+			}
+		}
+		if (!is_num)
+		{
+			FILE2 << line << endl;
+		}
+	}
+	FILE2.seekg(0, ios::beg);
+	int A_line_counter = 0;
+	while (getline(FILE2, line))
+	{
+		if (line[0] == 'А' || line[0] == 'A')
+		{
+			A_line_counter++;
+		}
+	}
+	cout << "Количество строк начинающихся с буквы А: " << A_line_counter << endl;
+
+}
---
+++ Lab3/Lab3E8.cpp
@@ -0,0 +1,50 @@
+﻿#include<iostream>
+#include<fstream>
+#include<Windows.h>
+#include<string>
+using namespace std;
+int main()
+{
+	string line;
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "Введите строку: " << endl;
+	getline(cin, line);
+	fstream file("file.txt");
+	if (!file.is_open())
+	{
+		cerr << "Ошибка открытия file.txt! " << endl;
+		return 1;
+	}
+	file << line << '\0';
+	file.seekg(0, ios::beg);
+	int start_index, end_index;
+	int max_syms = 0;
+	getline(file, line);
+	file.close();
+	int sym_counter = 0;
+	for (int i = 0; i < line.length(); i++)
+	{
+		if (line[i] >= 'A' && line[i] <= 'z' || line[i] >= 'А' && line[i] <= 'я')
+		{
+			while (line[i] != ' ' && line[i] != '\0')
+			{
+				i++;
+				sym_counter++;
+			}
+		}
+		if (sym_counter > max_syms)
+		{
+			end_index = i - 1;
+			start_index = end_index - sym_counter + 1;
+			max_syms = sym_counter;
+		}
+		sym_counter = 0;
+			
+	}
+	cout << "Количество символов самого длинного слова: " << max_syms << endl;
+	for (int i = start_index; i <= end_index; i++)
+	{
+		cout << line[i];
+	}
+}
